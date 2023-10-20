cefa436f51cc14e94f7267cc1d3701a26bac305b
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-20 17:38:51 +0530

    secure application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { IssueSchema } from "../../validationSchema";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: NextResponse) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
   const validation = IssueSchema.safeParse(body);
 
