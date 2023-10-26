bbc937cc2f9052283376ab8a13b32eff48ab91e3
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-26 19:15:06 +0530

    enchance the API to assign an issue

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,5 +2,21 @@ import { z } from "zod";
 
 export const IssueSchema = z.object({
   title: z.string().min(1, "Title is Required").max(255),
-  description: z.string().min(1, "Description is Required"),
+  description: z.string().min(1, "Description is Required").max(255),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is Required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is Required")
+    .max(65535)
+    .optional(),
+
+  assignedToUserId: z
+    .string()
+    .min(1, "Asssigned to UserId is Required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
