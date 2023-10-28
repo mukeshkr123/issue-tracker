a1fb9318ee741b042292a0cf82f41f68e62674ea
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-28 18:10:31 +0530

    Assign an isssue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,12 +2,12 @@
 
 import React, { useEffect, useState } from "react";
 import { Select } from "@radix-ui/themes";
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import axios from "axios";
 import { useQuery } from "react-query";
 import Skeleton from "@/app/components/Skeleton";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -24,13 +24,21 @@ const AssigneeSelect = () => {
   if (error) return null;
 
   return (
-    <Select.Root>
+    <Select.Root
+      defaultValue={issue.assignedToUserId || " "}
+      onValueChange={(userId) => {
+        axios.patch("/api/issues/" + issue.id, {
+          assignedToUserId: userId || null,
+        });
+      }}
+    >
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
+          <Select.Item value=" ">Unassigned</Select.Item>
           {users?.map((user) => (
-            <Select.Item key={user.id} value="1">
+            <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
           ))}
